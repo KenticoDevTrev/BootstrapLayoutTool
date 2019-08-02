0a5d780c7afb594f2c7faccbbfa0d2b7193803e2
Trevor Fayas <tfayas@hbs.net>
2019-08-02 11:01:32 -0500

    Updating repository to use new compiled view format

--- BootstrapLayoutTool.Views/Properties/AssemblyInfo.cs
+++ BootstrapLayoutTool.Views/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using CMS;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bootstrap Layout Tool")]
@@ -16,22 +16,22 @@ using CMS;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyDiscoverable]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ee15a184-fc0a-46e0-9f37-0e77c8f507fc")]
+[assembly: Guid("0f97b458-bf55-42a9-b90a-26dce87c9856")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("12.29.0.2")]
-[assembly: AssemblyFileVersion("12.29.0.2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
