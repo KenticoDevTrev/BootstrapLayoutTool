ef122248db18c4c9c1353f257152274773ff0069
Trevor Fayas <tfayas@hbs.net>
2019-08-07 16:35:24 -0500

    Added changes that didn't commit

--- BootstrapLayoutTool.Views/Properties/AssemblyInfo.cs
+++ BootstrapLayoutTool.Views/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using CMS;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bootstrap Layout Tool")]
-[assembly: AssemblyDescription("Bootstrap 4 based Section tool for Kentico MVC Page Builder")]
+[assembly: AssemblyTitle("Bootstrap Layout Tool with Page Builder Container")]
+[assembly: AssemblyDescription("Bootstrap 4 based Section tool for Kentico MVC Page Builder with Page Builder Containers")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Heartland Business Systems")]
 [assembly: AssemblyProduct("Bootstrap Layout Tool")]
